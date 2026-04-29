ed24de77ee6bc572bba7a100abda5e845c9883e4
exobchix <ing.hector.bta@gmail.com>
2026-04-29 00:06:53 -0600

    cambio de favicon y otras correcciones

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Exobchix | Ing. Héctor René Bautista Odilón';
+export const SITE_TITLE = 'Ing. Héctor René Bautista Odilón';
 export const SITE_DESCRIPTION = 'Personal Portfolio Website built with Astro and TailwindCSS.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
